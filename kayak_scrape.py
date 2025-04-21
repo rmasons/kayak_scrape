1097ea36ccb698520e44232289eb09cfafb9f80b
Mason S Russell <masonsrussell@outlook.com>
2025-04-20 22:09:02 -0400

    Updates spans and class attributes

--- kayak_scrape.py
+++ kayak_scrape.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup as BS
 from tqdm.auto import tqdm
-import dotenv, time
+import time
 import pandas as pd
 import numpy as np
 
 def get_flts(url) -> list:
     driver = webdriver.Chrome()
+    #driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.get(url)
     time.sleep(20)
     soup = BS(driver.page_source, 'lxml')
@@ -17,7 +18,7 @@ def clean_flts(flts_list) -> pd.DataFrame:
     for flt in tqdm(flts_list, 'Cleaning Flights..', leave=False):
         spans = flt.findAll('span')
         airlines = flt.find_all('div', attrs={'dir' : 'auto'})
-        prices = flt.find_all('div', attrs={'class' : 'f8F1-price-text'})
+        prices = flt.find_all('div', attrs={'class' : 'e2GB-price-text'})
         cabins = flt.find_all('div', attrs={'class' : 'DOum-name DOum-mod-ellipsis'})
         href_list = list(set([x['href'] for x in flt.find_all('a', attrs={'class' : 'oVHK-fclink'})]))
         ob_stops = 0 if flt.find_all('span', attrs={'class' : 'JWEO-stops-text'})[0].getText() == 'nonstop' else int(flt.find_all('span', attrs={'class' : 'JWEO-stops-text'})[0].getText()[0])
@@ -27,18 +28,18 @@ def clean_flts(flts_list) -> pd.DataFrame:
         if ob_stops > 1 or rtrn_stops > 1: pass
         for cabin in range(0, len(cabins)): 
             clnd_flts.append({
-                'ob_dep_tm' : spans[3].getText()
-                ,'ob_arr_tm' : spans[5].getText()
+                'ob_dep_tm' : spans[2].getText()
+                ,'ob_arr_tm' : spans[4].getText()
                 ,'ob_airline' : airlines[0].getText().split(' ')[0]
                 ,'ob_stops' : ob_stops
-                ,'ob_orig_arpt' : spans[8 + ob_offset].getText()
-                ,'ob_dest_arpt' : spans[11 + ob_offset].getText()
-                ,'rtrn_dep_tm' : spans[14 + ob_offset].getText()
-                ,'rtrn_arr_tm' : spans[16 + ob_offset].getText()
+                ,'ob_orig_arpt' : spans[7 + ob_offset].getText()
+                ,'ob_dest_arpt' : spans[10 + ob_offset].getText()
+                ,'rtrn_dep_tm' : spans[13 + ob_offset].getText()
+                ,'rtrn_arr_tm' : spans[15 + ob_offset].getText()
                 ,'rtrn_airline' : airlines[1].getText().split(' ')[0]
                 ,'rtrn_stops' : rtrn_stops
-                ,'rtrn_orig_arpt' : spans[19 + rtrn_offset].getText()
-                ,'rtrn_dest_arpt' : spans[22 + rtrn_offset].getText()
+                ,'rtrn_orig_arpt' : spans[18 + rtrn_offset].getText()
+                ,'rtrn_dest_arpt' : spans[21 + rtrn_offset].getText()
                 ,'cabin' : cabins[cabin].getText()
                 ,'fare' : prices[cabin].getText()
                 ,'bkng_link' : f'https://www.kayak.com{href_list[cabin]}'
